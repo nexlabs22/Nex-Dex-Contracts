3575f6788d1369d0ae849e55d1b01ba01ac29a76
Morteza3123 <mkhedry3123@gmail.com>
2022-11-25 20:44:14 +0330

    solving etherscan warnings

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -125,6 +125,15 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
+        version: "0.8.17",
+        settings: {
+          optimizer: {
+            runs: 200,
+            enabled: true
+          }
+        }
+      },
+      {
         version: "0.8.13",
         settings: {
           optimizer: {
