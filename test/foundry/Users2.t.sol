903c6521ceececbfda0bf2445d2d0c30cc4e1253
Morteza-Khedri <mkhedry3123@gmail.com>
2023-03-27 15:48:13 +0330

    test with 2 users

---
+++ test/foundry/Users2.t.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: UNLICENSED
+// pragma solidity ^0.8.13;
+pragma solidity ^0.8.17;
+
+import "forge-std/Test.sol";
+import "../../contracts/Exchange.sol";
+import "../../contracts/Token.sol";
+import "../../contracts/test/MockV3Aggregator.sol";
+import "./helper.sol";
+
+contract Users2 is Test {
+    
+    MockV3Aggregator public nftOracle;
+    MockV3Aggregator public ethPriceOracle;
+    Token public usdc;
+
+    Exchange public exchange;
+    Helper public helper;
+
+    address add1 = vm.addr(1);
+    address add2 = vm.addr(2);
+
+    function setUp() public {
+        usdc = new Token(1000000e18);
+        nftOracle = new MockV3Aggregator(18, 1e18);
+        ethPriceOracle = new MockV3Aggregator(18, 1300e18);
+        exchange = new Exchange(
+            address(nftOracle),
+            address(ethPriceOracle),
+            address(usdc)
+        );
+        helper = new Helper(
+            address(exchange),
+            address(nftOracle),
+            address(ethPriceOracle),
+            address(usdc)
+        );
+        exchange.initialVirtualPool(1e18);
+        usdc.transfer(add1, 1000e18);
+        usdc.transfer(add2, 1000e18);
+    }
+
+
+    function testActions() public {
+        //user 1 open 1000 usd long postion
+       vm.startPrank(add1);
+       usdc.approve(address(exchange), 1000e18);
+       exchange.depositCollateral(1000e18);
+       assertEq(usdc.balanceOf(address(add1)), 0);
+       assertEq(exchange.collateral(address(usdc), address(add1)), 1000e18);
+       exchange.openLongPosition(1000e18, 0);
+        vm.stopPrank();
+       //user 2 open 500 usd long postion
+       vm.startPrank(add2);
+       usdc.approve(address(exchange), 1000e18);
+       exchange.depositCollateral(1000e18);
+       assertEq(usdc.balanceOf(address(add2)), 0);
+       assertEq(exchange.collateral(address(usdc), address(add2)), 1000e18);
+       exchange.openLongPosition(1600e18, 0);
+        vm.stopPrank();
+       //user 1 close poition
+       vm.startPrank(add1);
+       exchange.closePositionComplete(0);
+       vm.stopPrank();
+       //user 1 close poition
+       vm.startPrank(add1);
+       exchange.closePositionComplete(0);
+       vm.stopPrank();
+    }
+
+    
+    
+}
